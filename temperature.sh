3c867f2005b1e034928e8f5c878db4f1e7efc333
Alex Kim <alexkim92@ksu.edu>
2018-09-12 10:48:29 -0500

    added Kelvin conversion to temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,1 +1,7 @@
 expr "($1 -32) * 5 / 9.0" | bc -l
+
+# added below codes to convert degrees F into degrees K
+echo Please type in the temperature in degrees Fahrenheit:
+read temp
+k=`echo "scale=3;($temp + "459.67") * 5/9"| bc`
+echo $temp degrees Fahrenheit in degrees Kelvin is: $k degrees Kelvin
